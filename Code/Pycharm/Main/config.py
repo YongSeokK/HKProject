8c634342cb02450329ff6195ca19fbdfdd65f2fa
KellooN <sonmj2026@naver.com>
2022-07-29 00:24:10 +0900

    Flask - config 수정 (07.29)

--- Code/Pycharm/Main/config.py
+++ Code/Pycharm/Main/config.py
@@ -6,14 +6,11 @@ DB_PORT = '3306'
 
 SECRET_KEY = 'dev'
 
-# ## 초기 DATA관련 init
-# RecipeData_Path = 'C:\\G_Project\\Code\\Pycharm\\Main\\result.json'
-# VolumeData_Path = 'C:\\G_Project\\Code\\Pycharm\\Main\\농산물거래량(48)_휴일제거.json'
 ## 초기 DATA관련 init
-Retail_FolderPath = "C:\\G_Project\\Code\\Pycharm\\Main\\testData\\Init\\retail_json\\"
-RecipeData_FilePath = "C:\\G_Project\\Code\\Pycharm\\Main\\testData\\Init\\result.json"
-WholesaleVolume_FilePath = "C:\\G_Project\\Code\\Pycharm\\Main\\testData\\Init\\220726_농산물거래량.json"
-WholesalePrice_FilePath = "C:\\G_Project\\Code\\Pycharm\\Main\\testData\\Init\\220726_농산물가격.json"
+Retail_FolderPath = "C:\\G_Project\\Code\\Pycharm\\Main\\ProjectData\\Init\\retail_json\\"
+RecipeData_FilePath = "C:\\G_Project\\Code\\Pycharm\\Main\\ProjectData\\Init\\result.json"
+WholesaleVolume_FilePath = "C:\\G_Project\\Code\\Pycharm\\Main\\ProjectData\\Init\\220726_농산물거래량.json"
+WholesalePrice_FilePath = "C:\\G_Project\\Code\\Pycharm\\Main\\ProjectData\\Init\\220726_농산물가격.json"
 
 ## Daily DATA관련 init
-Daily_FolderPath = 'C:\\G_Project\\Code\\Pycharm\\Main\\testData\\Daily\\'
+Daily_FolderPath = 'C:\\G_Project\\Code\\Pycharm\\Main\\ProjectData\\Daily\\'
