e30ffc994ebeb52541ad80b4a5c93a3a77763e25
KellooN <sonmj2026@naver.com>
2022-08-12 10:36:16 +0900

    Flask - source 최신화 (08.12)

--- Code/Pycharm/Main/day.py
+++ Code/Pycharm/Main/day.py
@@ -1,15 +1,16 @@
-from config import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, Area_Dict, Whole_List, Daily_FolderPath, Root_Path
+from config import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME, Root_Path, Area_Dict, Whole_List, Daily_FolderPath
 from server.define.daily_DB import DailyDB
 from server.prophet.parameter import W_Parameter_Dict, R_Parameter_Dict
 from server.prophet.prophet_code import MyProphet
 
-Daily = DailyDB(DB_USERNAME, DB_HOST, DB_PASSWORD, DB_NAME, Area_Dict, Whole_List, Daily_FolderPath)
+DailyDB = DailyDB(DB_USERNAME, DB_HOST, DB_PASSWORD, DB_NAME, Area_Dict, Whole_List, Daily_FolderPath)
 
-Daily_Prophet = MyProphet(DB_USERNAME, DB_HOST, DB_PASSWORD, DB_NAME, Root_Path, W_Parameter_Dict, R_Parameter_Dict)
+MyProphet = MyProphet(DB_USERNAME, DB_HOST, DB_PASSWORD, DB_NAME, W_Parameter_Dict, R_Parameter_Dict, Root_Path)
 
-# Daily_Check = Daily.Daily_Data()
-# print(Daily_Check)
-# Daily_Wholesale = Daily_Prophet.Wholesale()
-# Daily_Retail = Daily_Prophet.Retail()
-# print(Daily_Wholesale)
-# print(Daily_Retail)
+Daily_Check = DailyDB.DailyData()
+print(Daily_Check)
+
+Daily_Wholesale = MyProphet.Wholesale()
+print(Daily_Wholesale)
+Daily_Retail = MyProphet.Retail()
+print(Daily_Retail)
