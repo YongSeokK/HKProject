74075aea25b8f2970ce423252a3a148acdde4ad7
KellooN <sonmj2026@naver.com>
2022-08-18 02:17:04 +0900

    Flask - chatbot 병합 (08.18)

--- Code/Pycharm/Main/server/views/chatbot_views.py
+++ Code/Pycharm/Main/server/views/chatbot_views.py
@@ -24,29 +24,7 @@ from ..define.yolo5 import Yolorun
 bp = Blueprint('kakao_chatbot', __name__, url_prefix='/kakao_chatbot')
 
 
-## 웰컴 인사
-@bp.route('/welcome', methods=['POST', 'GET'])
-def welcome():
-    ret = {
-        "version": "2.0",
-        "template": {
-            "outputs": [{
-                "simpleText": {
-                    "text": """
-내일 뭐먹지 챗봇을 이용해주셔서 감사합니다. 
-아래의 사용방법 버튼을 클릭하여,
-사용법을 숙지하신 후,
-서비스를 이용해주시기 바랍니다. 
-감사합니다.
-                                         """
-                }
-            }]
-        }
-    }
-    return jsonify(ret)
-
-
-## 소개
+## init (사용법)
 @bp.route('/init', methods=['POST', 'GET'])
 def init():
     ret = {
@@ -59,23 +37,23 @@ def init():
 
 1. 하단의 '레시피 검색' 클릭
 
-    1-1. 이미지로 찾기 ❗모바일 만❗
+    1-1. 이미지로 찾기 ❗모바일❗
     👉 '이미지' 클릭
     👉 '이미지 업로드' 클릭
     👉 찾고 싶은 음식 사진 전송
-    
     1-2. 텍스트로 찾기
     👉 '텍스트' 클릭
     👉 채팅창에 음식명 입력
-    
     1-3. '재료 확인'
     👉 음식의 재료와 가격 출력
-    
     1-4. '레시피 보기'
     👉 레시피 링크 접속
     
+    
+    
 2. '농수산물 가격 정보'
     👉 찾고 싶은 농수산물 입력
+    
     
 맛있게 요리해드세요. 😃 """
                 }
@@ -106,7 +84,6 @@ def find_img():
     UserInfo = req_json['userRequest']['user']['id']  # UserInfo 는 유저의 아이디값
 
     dir_path = ("C:\\Users\\hkedu\\HKProject\\Code\\Pycharm\\Main\\server\\static\\upload_img\\")
-    print('dir_path: ', glob(dir_path))
     if os.path.exists(dir_path):
         if len(glob(dir_path + '\\*')) != 0:
             for file in glob(dir_path + '\\*'):
@@ -114,8 +91,8 @@ def find_img():
     cnt = 1
     for i in URLList:
         urllib.request.urlretrieve(i,
-                                   "C:\\Users\\hkedu\\HKProject\\Code\\Pycharm\\Main\\server\\static\\upload_img\\" + str(
-                                       UserInfo) + "food" + str(cnt) + ".jpg")
+                                   "C:\\Users\\hkedu\\HKProject\\Code\\Pycharm\\Main\\server\\static\\upload_img\\" +
+                                   str(UserInfo) + "food" + str(cnt) + ".jpg")
         cnt += 1
 
     # 위의 코드는 URL주소를 이용하여 로컬피시에 저장
@@ -193,7 +170,7 @@ def find_img():
                 with db.cursor() as cur:
                     sql_table = 'SELECT * FROM food_recipe WHERE dish LIKE "%{}%" ORDER BY views DESC LIMIT 3;'.format(
                         Yolorun_return[0])
-                    # SELECT 선택 = food_recipe테이블, WHERE = dish컬럼, LIKE = .format(x[0])과 같은 형태로, DESC = 내림차순, LIMIT = 제한 3개
+                    # SELECT 선택 = food_recipe 테이블, WHERE = dish컬럼, LIKE = .format(x[0])과 같은 형태로, DESC = 내림차순, LIMIT = 제한 3개
 
                     cur.execute(sql_table)
                     # 가상의공간에 sql_table를 실행
@@ -223,7 +200,7 @@ def find_img():
                                 "title": "요리 양",
                                 "description": SQLFOOD_list[cnt][9]
                             }, {
-                                "title": "평균 조리시간",
+                                "title": "조리시간",
                                 "description": SQLFOOD_list[cnt][11]
                             }, {
                                 "title": "조회수",
@@ -243,11 +220,11 @@ def find_img():
                                 "label": "레시피 보기",
                                 "action": "webLink",
                                 "webLinkUrl": URL + str(SQLFOOD_list[cnt][1])
-                            }]
+                            }],
+                            "buttonLayout": "vertical"
                         }
                         yolochat_List.append(yolochat)
                     cur.close()
-            print(Yolorun_return[0])
             # ret은 카톡챗봇의 응답
             ret = {
                 "version": "2.0",
@@ -283,7 +260,7 @@ def find_ingredients():
     ret = request.get_json()
     ingredient_txt = ret['action']['clientExtra']['Food_Recipe1']
     ingredient_list = ingredient_txt.split(',')
-    tmp = '현재 레시피에 들어가는 재료 입니다. \n 추가로 네이버 쇼핑으로 재료를 구매 하고 싶으면 아래 버튼을 이용해 주세요. \n'
+    tmp = '🍽 레시피 재료 🍽 \n'
     for cnt, data in enumerate(ingredient_list):
         # print(data)
         ingredients = data.split('_')[0]
@@ -305,7 +282,7 @@ def find_ingredients():
             "quickReplies": [{
                 "messageText": "네이버 쇼핑",
                 "action": "message",
-                "label": "네이버에서 구매하기"
+                "label": "네이버에서 재료 구매하기"
             }, {
                 "messageText": "텍스트 검색",
                 "action": "message",
@@ -347,7 +324,8 @@ def find_txt():
                         "quickReplies": [{
                             "messageText": "텍스트 검색",
                             "action": "message",
-                            "label": "텍스트 다시 작성하기"}, {
+                            "label": "텍스트 다시 작성하기"
+                        }, {
                             "messageText": "레시피 검색",
                             "action": "message",
                             "label": "뒤로 돌아가기"
@@ -363,26 +341,19 @@ def find_txt():
                         },
                         "title": "간단 소개",
                         "description": SQLFOOD_list[cnt][7],
-                        "thumbnail": {
-                            "imageUrl": SQLFOOD_list[cnt][12],
-                            "width": 800,
-                            "height": 800},
-                        "itemList": [{
-                            "title": "요리 난이도",
-                            "description": SQLFOOD_list[cnt][10]
-                        }, {
-                            "title": "테마",
-                            "description": SQLFOOD_list[cnt][6]
-                        }, {
-                            "title": "요리 양",
-                            "description": SQLFOOD_list[cnt][9]
-                        }, {
-                            "title": "평균 조리시간",
-                            "description": SQLFOOD_list[cnt][11]
-                        }, {
-                            "title": "조회수",
-                            "description": SQLFOOD_list[cnt][4]
-                        }, ],
+                        "thumbnail": {"imageUrl": SQLFOOD_list[cnt][12],
+                                      "width": 800,
+                                      "height": 800},
+                        "itemList": [{"title": "요리 난이도",
+                                      "description": SQLFOOD_list[cnt][10]},
+                                     {"title": "테마",
+                                      "description": SQLFOOD_list[cnt][6]},
+                                     {"title": "요리 양",
+                                      "description": SQLFOOD_list[cnt][9]},
+                                     {"title": "평균 조리시간",
+                                      "description": SQLFOOD_list[cnt][11]},
+                                     {"title": "조회수",
+                                      "description": SQLFOOD_list[cnt][4]}, ],
                         "itemListAlignment": "right",
                         "buttons": [{
                             "label": "재료 확인",
@@ -432,7 +403,7 @@ def find_txt():
             return jsonify(ret)
 
 
-## fnaver_shop (네이버 쇼핑)
+## naver_shop (네이버 쇼핑)
 @bp.route('/naver_shop', methods=['POST', 'GET'])
 def naver_shop():
     req_json = request.get_json()
@@ -459,29 +430,14 @@ def naver_shop():
                 }]
             }
         }
-
     else:
         Result_Lsit = []
         for data in dataList:
             data_dict = {
-                "imageTitle": {
-                    "title": data['이름'],
-                },
-                "thumbnail": {
-                    "imageUrl": data['이미지'],
-                    "width": 800,
-                    "height": 800
-                },
-                "itemList": [{
-                    "title": "가격",
-                    "description": data['최저가격'] + "원"
-                }],
-                "itemListAlignment": "left",
-                "buttons": [{
-                    "action": "webLink",
-                    "label": "쇼핑하러 가기",
-                    "webLinkUrl": data['url']
-                }]
+                "title": data['이름'],
+                "description": data['최저가격'] + "원",
+                "imageUrl": data['이미지'],
+                "link": {"web": data['url']}
             }
 
             Result_Lsit.append(data_dict)
@@ -490,9 +446,16 @@ def naver_shop():
             "version": "2.0",
             "template": {
                 "outputs": [{
-                    "carousel": {
-                        "type": "itemCard",
-                        "items": Result_Lsit
+                    "listCard": {
+                        "header": {
+                            "title": "네이버 쇼핑 " + temp + " 상위 3개"
+                        },
+                        "items": Result_Lsit,
+                        "buttons": [{
+                            "label": "더 많은 판매자 보기",
+                            "action": "webLink",
+                            "webLinkUrl": "https://search.shopping.naver.com/search/all?query=" + temp,
+                        }]
                     }
                 }],
                 "quickReplies": [{
@@ -509,6 +472,7 @@ def naver_shop():
     return jsonify(ret)
 
 
+## retail_price (소매 가격 정보)
 @bp.route('/retail_price', methods=['POST', 'GET'])
 def retail_price():
     req_json = request.get_json()
@@ -531,59 +495,27 @@ def retail_price():
             Cprice = abs(Cprice)
             Cprice = str(Cprice).split('.')[0]
             print(Cprice)
-            info = '원 올랐습니다.'
-        else:
+            info1 = '전날 대비, '
+            info2 = '원 상승 😢'
+            Result = info1 + Cprice + info2
+        elif Cprice < 0:
             Cprice = abs(Cprice)
             Cprice = str(Cprice).split('.')[0]
-            info = '원 내렸습니다.'
+            info1 = '전날 대비, '
+            info2 = '원 하락 😄'
+            Result = info1 + Cprice + info2
+        else:
+            Result = '전날과 동일 😀'
         day = str(round(day)).split('.')[0]
-        ret = {
-            "version": "2.0",
-            "template": {
-                "outputs": [{
-                    "simpleText": {
-                        "text":
-                            Month + ' 월 ' + Day + ' 일 ' + temp + '의 평균 가격은 ' + day + '원 이며,\n전날과 비교하여, ' + Cprice + info
-                    }
-                }],
-                "quickReplies": [{
-                    "messageText": "이미지 검색",
-                    "action": "message",
-                    "label": "이미지 검색"
-                }, {
-                    "messageText": "텍스트 검색",
-                    "action": "message",
-                    "label": "텍스트 검색"
-                }, {
-                    "messageText": "농수산물 가격 정보 확인하기",
-                    "action": "message",
-                    "label": "농수산물 가격 검색"
-                }, ]
-            }
-        }
+        ret = {"version": "2.0", "template": {"outputs": [{"simpleText": {"text":
+                                                                              "❗ " + Month + '월 ' + Day + '일 평균 가격 기준 ❗\n' + temp + '의 평균 가격은 ' + day + '원\n' + Result}}],
+                                              "quickReplies": [{"messageText": "농수산물 가격 정보",
+                                                                "action": "message",
+                                                                "label": "다른 농수산물 검색하기"}, ]}}
     else:
-        ret = {
-            "version": "2.0",
-            "template": {
-                "outputs": [{
-                    "simpleText": {
-                        "text": '해당 작물에 대한 정보가 없습니다.'
-                    }
-                }],
-                "quickReplies": [{
-                    "messageText": "이미지 검색",
-                    "action": "message",
-                    "label": "이미지 검색"
-                }, {
-                    "messageText": "텍스트 검색",
-                    "action": "message",
-                    "label": "텍스트 검색"
-                }, {
-                    "messageText": "농수산물 가격 정보 확인하기",
-                    "action": "message",
-                    "label": "농수산물 가격 검색"
-                }, ]
-            }
-        }
+        ret = {"version": "2.0", "template": {"outputs": [{"simpleText": {"text": '해당 작물에 대한 정보가 없습니다.'}}],
+                                              "quickReplies": [{"messageText": "농수산물 가격 정보",
+                                                                "action": "message",
+                                                                "label": "다른 농수산물 검색하기"}, ]}}
 
     return jsonify(ret)
